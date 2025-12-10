6b566c3f579118f047ba2683f68e98cf9ae5316c
elber.castillo <elber.castillo@correounivalle.edu.co>
2025-12-10 04:07:43 +0000

    TypeScript Class:
    Check the app.ts file for the changes required to complete the task assigned.

--- app.ts
+++ app.ts
@@ -3,10 +3,47 @@ to create various files in the same project using similar names
 */
 
 (() => {
-    const a = 10; // Wrong!
-    let b:number = 20; // Correct!!
-    
-    function sayHello( msg:string ) {
-        console.log(msg)
+
+    /**
+     * The task is to type correctly the following code
+     */
+
+    // Types
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuple
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean]  = ['Lex Lutor',5,true];
+  
+    // Array
+    const aliados:(string)[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enums
+
+    enum HeroPower {
+        fuerzaAcuaman = 0,
+        fuerzaBatman = 1,
+        fuerzaFlash = 5,
+        fuerzaSuperman = 100,
     }
-})()
+
+    // Function Returns
+    function activar_batiseñal(): string {
+      return 'activada';
+    }
+  
+    function pedir_ayuda(): void {
+      console.log('Auxilio!!!');
+    }
+  
+    // Type Asercions
+    const poder: any = '100';
+    const largoDelPoder:number = Number(poder.length);
+    console.log( largoDelPoder );
+  
+  })()
+  
+  
--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,36 @@
 to create various files in the same project using similar names
 */
 (function () {
-    var a = 10; // Wrong!
-    var b = 20; // Correct!!
-    function sayHello(msg) {
-        console.log(msg);
+    /**
+     * The task is to type correctly the following code
+     */
+    // Types
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    // Tuple
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    // Array
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enums
+    var HeroPower;
+    (function (HeroPower) {
+        HeroPower[HeroPower["fuerzaAcuaman"] = 0] = "fuerzaAcuaman";
+        HeroPower[HeroPower["fuerzaBatman"] = 1] = "fuerzaBatman";
+        HeroPower[HeroPower["fuerzaFlash"] = 5] = "fuerzaFlash";
+        HeroPower[HeroPower["fuerzaSuperman"] = 100] = "fuerzaSuperman";
+    })(HeroPower || (HeroPower = {}));
+    // Function Returns
+    function activar_batiseñal() {
+        return 'activada';
     }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Type Asercions
+    var poder = '100';
+    var largoDelPoder = Number(poder.length);
+    console.log(largoDelPoder);
 })();
 //# sourceMappingURL=app.js.map
