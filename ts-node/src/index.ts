c25ca169f64d4222509eb65d58cdf9087f3908a4
elber.castillo <elber.castillo@correounivalle.edu.co>
2026-02-25 01:51:50 +0000

    TypeScript Class 10:
    - Why do we need namespaces?
    - Create namespaces
    - Multiples namespaces in the same project.
    - Import/Export namespaces

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,8 @@
 import { Hero } from './classes/Hero';
-// import { Hero as SuperHero, Hero2 } from './classes/Hero';
-// import * as HeroClasses from './classes/Hero';
+import powers from './data/powers';
 
-
-// const ironman = new SuperHero('Ironman', 1, 55);
-const ironman = new Hero('Ironman', 10, 55);
-
+const ironman = new Hero('Ironman', 1, 55);
 console.log( ironman );
-console.log( ironman.power );
+console.log( "Power: " + ironman.power );
+
+console.log(powers)
