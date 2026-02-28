a47d22808f432ca8923388cea6be075e9a347ea7
elber.castillo <elber.castillo@correounivalle.edu.co>
2026-02-28 00:05:44 +0000

    TypeScript Class 12:
    - What are decorators, what are they used for?
    - Class decorators
    - Factory decorators
    - Method decorator
    - Property decorator
    - Parameter decorator

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,9 +1,33 @@
-import { getPokemon } from "../generics/get-pokemon";
+import { Pokemon } from "../decorators/classDecorator";
+import { Digimon } from "../decorators/factoryDecorator";
+import { ExamplePokemon } from "../decorators/blockDecorator";
+import { PokemonDB } from "../decorators/methodDecorator";
+import { PokemonPrivate } from "../decorators/propertyDecorator";
+
+const bulbasaur = new Pokemon("Bulbasaur");
+console.log(bulbasaur)
+
+const agumon = new Digimon("Agumon");
+console.log(agumon)
+
+const charmander = new ExamplePokemon("Charmander");
+/**
+ * This would change the class prototype to include a "customName" param 
+ * with value of "Charizard", as the decorator has been "blocked", this
+ * edition won't be performed and the class will be intact.
+ */
+(ExamplePokemon.prototype as any).customName = 'Charizard';
+console.log(charmander)
+
+const pikachu = new PokemonDB("Pikachu");
+pikachu.savePokemonToDB(25);
+console.log(pikachu)
 
 
-getPokemon(1)
-    .then( 
-        pokemon => console.log(pokemon.name, pokemon.sprites.front_default) 
-    )
-    .catch( error => console.error(error))
-    .finally( () => console.log("End of getPokemon") )
+const mewtwo = new PokemonPrivate("Mewtwo");
+/**
+ * Although is a private parameter, this would edit the 
+ * API route, damagin the class declaration
+ */
+// mewtwo.publicAPI = "https://pokemondb.net/pokedex/mewtwo"
+console.log(mewtwo)
